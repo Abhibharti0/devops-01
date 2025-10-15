155e5232b41d5dde4a6adf1c9b95b36e1d3a4189
Abhishek Bharti <abhibharti457@gmail.com>
2025-10-15 21:46:03 +0530

    ec2 check

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,7 +2,7 @@
 WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-ok
+ec2 check
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
